daa42aa3bc249afa19eb9c6a0dca4809ea084b80
bavaz1 <kismadar12@gmail.com>
2017-11-22 10:44:16 +0100

    mysqldump added

---
+++ flowadventures.sql
@@ -0,0 +1,140 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: Flow_Adventures
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Game_areas`
+--
+
+DROP TABLE IF EXISTS `Game_areas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Game_areas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `x_coordinate` int(11) DEFAULT NULL,
+  `y_coordinate` int(11) DEFAULT NULL,
+  `gamefield_id` int(11) DEFAULT NULL,
+  `npc_id` int(11) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Game_areas`
+--
+
+LOCK TABLES `Game_areas` WRITE;
+/*!40000 ALTER TABLE `Game_areas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Game_areas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Gamefields`
+--
+
+DROP TABLE IF EXISTS `Gamefields`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Gamefields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  `description` text,
+  `far_description` varchar(255) DEFAULT NULL,
+  `access` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Gamefields`
+--
+
+LOCK TABLES `Gamefields` WRITE;
+/*!40000 ALTER TABLE `Gamefields` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Gamefields` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Items`
+--
+
+DROP TABLE IF EXISTS `Items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `STATUS` tinyint(1) DEFAULT NULL,
+  `description` text,
+  `health_modifier` int(11) DEFAULT NULL,
+  `damage_modifier` int(11) DEFAULT NULL,
+  `defense_modifier` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Items`
+--
+
+LOCK TABLES `Items` WRITE;
+/*!40000 ALTER TABLE `Items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `NPCs`
+--
+
+DROP TABLE IF EXISTS `NPCs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NPCs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `health` int(11) DEFAULT NULL,
+  `damage` int(11) DEFAULT NULL,
+  `defense` int(11) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `alive_description` text,
+  `dead_description` text,
+  `alive_far_description` text,
+  `dead_far_description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `NPCs`
+--
+
+LOCK TABLES `NPCs` WRITE;
+/*!40000 ALTER TABLE `NPCs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `NPCs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-22 10:42:35
