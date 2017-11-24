7854249aa161450f2f30eb7314190eba7f8c6b16
vitriolfox <vitriol.fox@gmail.com>
2017-11-24 10:41:21 +0100

    basic website created

--- express/index.js
+++ express/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('views', './views/');
+app.set('view engine', 'hbs');
+app.engine('hbs', require('hbs').__express);
+app.use(express.static('public'));
+
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index');
 });
+app.get('/play', function(req, res){
+  res.render('play');
+});
+
 
 app.listen(3000);
