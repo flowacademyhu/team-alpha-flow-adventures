8ae9cd6000e1228677969c825ff45b701ba7b60b
vitriolfox <vitriol.fox@gmail.com>
2017-11-24 11:39:04 +0100

    Npc model created

---
+++ server/models/Npc.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../services/sequelize.js').getInstance();
+
+const Npc = sequelize.define('npc', {
+  id: Sequelize.INTEGER,
+  name: Sequelize.STRING,
+  item_id: Sequelize.INTEGER,
+  health: Sequelize.INTEGER,
+  damage: Sequelize.INTEGER,
+  defense: Sequelize.INTEGER,
+  status: Sequelize.BOOLEAN,
+  alive_description: Sequelize.STRING,
+  dead_description: Sequelize.STRING,
+  alive_far_description: Sequelize.STRING,
+  dead_far_description: Sequelize.STRING,
+  speak: Sequelize.STRING
+});
+
+module.exports = Npc;
