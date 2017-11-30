becbd2d169f2805616295bb604a99e7771acb1a9
vitriolfox <vitriol.fox@gmail.com>
2017-11-30 10:13:41 +0100

    npc stats refactored

--- app/models/characters/npc/hostile/wild-boar.js
+++ app/models/characters/npc/hostile/wild-boar.js
@@ -5,9 +5,8 @@ const sharpTusk = require('../../../items/weapons/sharp-tusk');
 module.exports = {
   name: 'vadkan',
   hp: 15,
-  str: 8,
-  int: 1,
-  agi: 2,
+  dmg: 8,
+  def: 3,
   items: sharpTusk,
   aggro: true,
   text: 'Röff-röff uííí-uííí... RÖFF!'
