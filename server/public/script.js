617df480016ce55fa59f3c28eb42de86873946c7
bavaz1 <kismadar12@gmail.com>
2017-12-05 10:38:28 +0100

    north and south movement buttons implemented on client side

--- server/public/script.js
+++ server/public/script.js
@@ -1,12 +1,17 @@
 console.log('halihó');
 
 let newGameButton = document.querySelector('.new-game');
+let moveNorthButton = document.querySelector('.move-north');
+let moveSouthButton = document.querySelector('.move-south');
 
 let currentFieldDescription = document.querySelector('.field-desc');
 let northDescription = document.querySelector('.north-desc');
 let southDescription = document.querySelector('.south-desc');
 let eastDescription = document.querySelector('.east-desc');
 let westDescription = document.querySelector('.west-desc');
+
+let northMovement = document.querySelector('.move-north');
+let southMovement = document.querySelector('.move-south');
 
 function currentLocationDisplay (currentGameField) {
   currentFieldDescription.innerHTML = currentGameField.fieldDesc;
@@ -23,4 +28,20 @@ function newGame () {
     .catch(error => console.log(error));
 }
 
+function moveNorth () {
+  return fetch('/games/movements/north', {method: 'PUT'})
+    .then(response => response.json())
+    .then(data => currentLocationDisplay(data))
+    .catch(error => console.log(error));
+}
+
+function moveSouth () {
+  return fetch('/games/movements/south', {method: 'PUT'})
+    .then(response => response.json())
+    .then(data => currentLocationDisplay(data))
+    .catch(error => console.log(error));
+}
+
 newGameButton.addEventListener('click', newGame);
+moveNorthButton.addEventListener('click', moveNorth);
+moveSouthButton.addEventListener('click', moveSouth);
