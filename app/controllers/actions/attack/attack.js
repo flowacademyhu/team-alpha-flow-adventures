3abff540c19f268fa9dc8218a73dac6f7c8c813e
Stickz <vastag.balint@gmail.com>
2017-12-08 11:09:21 +0100

    refactored combat system

--- app/controllers/actions/attack/attack.js
+++ app/controllers/actions/attack/attack.js
@@ -4,37 +4,108 @@ const gameArea = require('../../../models/game-area/game-field/index');
 const position = require('../../../models/game-area/starter-position');
 const player = require('../../../models/characters/player');
 const random = require('./random/random');
+const output = require('../../../views/output');
 
-module.exports = function (npc, matrix) {
-  let n = 1;
-  let targetNpc = gameArea[matrix[position[1]][position[0]]].npc;
-  if (npc === undefined || npc !== targetNpc.name) {
-    console.log('Kit?');
-  }
-  if (targetNpc !== null && targetNpc.hp < 1) {
-    console.log('A(z) ' + targetNpc.name + ' halott, nincs kivel harcolnod.');
-  }
-  if (targetNpc !== null && targetNpc.hp > 0) {
-    if (gameArea[matrix[position[1]][position[0]]].npc.name === npc) {
-      while (targetNpc.hp > 0 && player.hp > 0) {
-        let playerRandomRoll = random(1, 6);
-        let npcRandomRoll = random(1, 6);
-        console.log(n + '. kör');
-        n++;
-        let playerDmg = (player.dmg + playerRandomRoll) - targetNpc.def;
-        let npcDmg = (targetNpc.dmg + npcRandomRoll) - player.def;
+let combatRoundCounter = 1;
+let targetNpc;
+let playerRandomRoll;
+let npcRandomRoll;
+let playerDamage;
+let npcDamage;
 
-        (playerDmg >= 0) ? targetNpc.hp -= playerDmg : targetNpc.hp -= 0;
-        console.log('Megtámadtad a ' + targetNpc.name + '-t ' + (playerDmg) + ' sebzéssel');
-        (npcDmg >= 0) ? player.hp -= npcDmg : player.hp -= 0;
-        console.log('Megtámadott a ' + targetNpc.name + ' ' + (npcDmg) + ' sebzéssel');
-        console.log('--------------------------------------');
-      }
-      console.log('Jelenlegi életerőd ' + player.hp + ' || ellenfeled hátramaradó életereje ' + targetNpc.hp);
+class Combat {
+  _isDeadandHasItem () {
+    return targetNpc.items !== null && targetNpc.hp <= 0;
+  }
+
+  _deadNpcOutput () {
+    output('A(z) ' + targetNpc.name + ' meghalt. Elejtette a(z) <span class=useable-items interactables">' +
+    targetNpc.items.name + '</span>-t');
+    output('--------------------------------------');
+  }
+
+  isNonExistentAttackTarget (npc) {
+    return npc === undefined || npc !== targetNpc.name;
+  }
+
+  _combatDiceRoll () {
+    playerRandomRoll = random(1, 6);
+    npcRandomRoll = random(1, 6);
+  }
+
+  _isDeadNpc () {
+    return targetNpc !== null && targetNpc.hp < 1;
+  }
+
+  _checkValidTargetName (npc, matrix) {
+    return gameArea[matrix[position[1]][position[0]]].npc.name === npc;
+  }
+
+  _combatContestantsAlive () {
+    return targetNpc.hp > 0 && player.hp > 0;
+  }
+
+  _getContestantsDamage () {
+    playerDamage = (player.dmg + playerRandomRoll) - targetNpc.def;
+    npcDamage = (targetNpc.dmg + npcRandomRoll) - player.def;
+  }
+
+  _checkValidDamage (npcDamage, playerDamage) {
+    (playerDamage >= 0) ? targetNpc.hp -= playerDamage : targetNpc.hp -= 0;
+    (npcDamage >= 0) ? player.hp -= npcDamage : player.hp -= 0;
+  }
+
+  _combatTextOutput () {
+    output('Megtámadtad a ' + targetNpc.name + '-t ' + (playerDamage) + ' sebzéssel');
+    output('Megtámadott a ' + targetNpc.name + ' ' + (npcDamage) + ' sebzéssel');
+    output('--------------------------------------');
+    output('Jelenlegi életerőd ' + player.hp + ' || ellenfeled hátramaradó életereje ' + targetNpc.hp);
+  }
+
+  _combatRoundMethodWrapper () {
+    this._combatDiceRoll();
+    output(combatRoundCounter + '. kör');
+    combatRoundCounter++;
+    this._getContestantsDamage();
+    this._combatTextOutput();
+  }
+  _NotvalidAttackTarget () {
+    if (this._nonExistentAttackTarget()) {
+      output('Kit?');
     }
-    if (targetNpc.items !== null && targetNpc.hp <= 0) {
-      console.log('A(z) ' + targetNpc.name + ' meghalt. Elejtette a(z) ' + '\x1b[33m' + targetNpc.items.name + '\x1b[0m' + '-t');
-      console.log('--------------------------------------');
+    if (this._isDeadNpc()) {
+      output('A(z) ' + targetNpc.name + ' halott, nincs kivel harcolnod.');
     }
   }
-};
+
+  _activeCombat () {
+    while (this.combatContestantsAlive()) {
+      this.combatRoundMethodWrapper();
+    }
+  }
+
+  _itemDropHandler () {
+    if (this.isDeadandHasItem()) {
+      this.deadNpcOutput();
+    }
+  }
+
+  validOnFieldTarget (npc, matrix) {
+    if (!this.isDeadNpc()) {
+      this._checkValidTargetName(npc, matrix);
+    }
+  }
+
+/* --------------------------- */
+  run (npc, matrix) {
+    this.npc = npc;
+    this.matrix = matrix;
+    this.targetNpc = gameArea[matrix[position[1]][position[0]]].npc;
+    this.NotvalidAttackTarget();
+    if (this.validOnFieldTarget(npc, matrix)) {
+      this._activeCombat();
+      this._itemDropHandler();
+    }
+  }
+}
+module.exports = Combat.run;
