8702e3d16fef5bc86f7f1f8bc48a07ce91ee574a
Stickz <vastag.balint@gmail.com>
2017-12-14 16:38:34 +0100

    wip ezt is parancsra tettem :(

--- server/models/Map.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../services/sequelize').getInstance();
-
-const Map = sequelize.define('Map', {
-  name: Sequelize.STRING,
-  author: sequelize.STRING
-});
-
-module.exports = Map;
